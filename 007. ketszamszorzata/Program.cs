52f2a4558189cd3efbedee1bc8ba6dc97eea6175
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-25 21:11:15 +0100

    Két szám szorzata

---
+++ 007. ketszamszorzata/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007.ketszamszorzata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot! ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérek még egy számot! ");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A két szám szorzata: " + szam * szam2 + ".");
+
+            Console.ReadKey();
+        }
+    }
+}
