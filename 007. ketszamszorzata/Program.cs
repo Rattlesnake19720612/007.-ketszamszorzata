b3f0fe3d21bdd73e5674ac3697f91ba69a2e5c3d
DGYKKSZKI <124397426+Rattlesnake19720612@users.noreply.github.com>
2023-05-15 13:11:00 +0200

    Két szám szorzata

--- 007. ketszamszorzata/Program.cs
+++ 007. ketszamszorzata/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,3 +20,20 @@ namespace _007.ketszamszorzata
         }
     }
 }
+
+/*Két egész számot kér be a felhasználótól, megszorozza őket egymással, majd kiírja az eredményt. Az alábbiakban röviden összefoglalom, hogy a program mit csinál:
+
+A program elindul, és a "using" utasításokkal importálja a szükséges könyvtárakat.
+
+Definiáljuk a "Program" osztályt, amely tartalmazza a "Main" metódust, ami a program belépési pontja.
+
+A "Main" metóduson belül a program egy felugró ablakban megjelenít egy üzenetet, ami arra kéri a felhasználót, hogy írjon be egy számot. A felhasználó által megadott értéket a "Console.ReadLine()" metódus segítségével szövegként olvassuk be, majd az "Convert.ToInt32()" metódussal integerré alakítjuk. Az eredményt a "szam" változóban tároljuk.
+
+A program ugyanazt az eljárást használja a második szám beolvasásához, amelyet a "szam2" változóban tárolunk.
+
+Az első és a második szám szorzata a "szam" és a "szam2" változók szorzásával számolódik ki, az eredményt a "Console.WriteLine()" metódus segítségével kiírjuk a konzolra.
+
+Végül a program várakozik, amíg a felhasználó bármilyen billentyűt meg nem nyom a "Console.ReadKey()" metódus segítségével.
+
+Összességében ez a program egy egyszerű példa arra, hogy hogyan lehet alapvető aritmetikai műveleteket végezni C# nyelven, és hogyan lehet a felhasználóval a konzolon keresztül kommunikálni.*/
+
